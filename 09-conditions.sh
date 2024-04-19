ae0468ec40516331dcd794a269801848a49913a6
niharika2211 <niharika2211@gmail.com>
2024-04-19 23:08:29 +0530

    Hello-script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,8 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -lt 50 ]
 then 
-    echo "given statement is true"
+    echo "given statement is true $NUMBER lessthan 50"
 else
-    echo "given statement is false"
+    echo "given statement is false $NUMBER greaterthan 50"
 
 fi
