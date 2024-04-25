5a64a7ddee1e624290fed6b5d4df5094e58629bd
niharika2211 <niharika2211@gmail.com>
2024-04-25 15:33:22 +0530

    Hello script

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,11 +2,11 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0 ]
+if [$USERID -ne 0]
  then 
-     echo "you are a root user"
- else
      echo "you are not a root user, execute with root access"
+ else
+     echo "you are a root user"
      exit 1 
 fi 
 
@@ -14,8 +14,8 @@ dnf install mysql -y
 
 if [$? -ne 0]
  then 
-    echo "installation of mysql is success..."
+    echo "installation of mysql failed..."
  else
-    echo "installation of mysql failed"
+    echo "installation of mysql is success..."
     exit 1 #execute with root access
 fi    
