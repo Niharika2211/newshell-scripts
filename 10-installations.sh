863e02d582fd66be34aa470bbc29863308aac791
niharika2211 <niharika2211@gmail.com>
2024-04-25 15:51:50 +0530

    Hello script

--- 10-installations.sh
+++ 10-installations.sh
@@ -3,31 +3,31 @@
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then
-    echo "Please run this script with root access."
-    exit 1 # manually exit if error comes.
-else
-    echo "You are super user."
-fi
+ then 
+     echo "you are not a root user, execute with root access"
+     exit 1 # manually exit if not a root user
+ else
+     echo "you are a root user"
+     
+fi 
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]
-then
-    echo "Installation of mysql...FAILURE"
-    exit 1
-else
-    echo "Installation of mysql...SUCCESS"
-fi
+ then 
+    echo "installation of mysql failed..."
+    exit 1 # error not a rootuser
+ else
+    echo "installation of mysql is success..."
+     #executed with root access
+fi    
 
 dnf install git -y
 
 if [ $? -ne 0 ]
-then
-    echo "Installation of git...FAILURE"
-    exit 1
-else
-    echo "Installation of Git...SUCCESS"
+ then 
+    echo "git installation failed..."
+    exit 1 # not a root user"
+ else 
+    echo "git installations success"
 fi
-
-echo "is script proceeding?"
--- 11-functions.sh
+++ 11-functions.sh
@@ -1,36 +1,4 @@
-#!/bin/bash
 
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
- then 
-     echo "you are not a root user, execute with root access"
-     exit 1 # manually exit if not a root user
- else
-     echo "you are a root user"
-     
-fi 
-
-dnf install mysql -y
-
-if [ $? -ne 0 ]
- then 
-    echo "installation of mysql failed..."
-    exit 1 # error not a rootuser
- else
-    echo "installation of mysql is success..."
-     #executed with root access
-fi    
-
-dnf install git -y
-
-if [ $? -ne 0 ]
- then 
-    echo "git installation failed..."
-    exit 1 # not a root user"
- else 
-    echo "git installations success"
-fi
 
 
 
