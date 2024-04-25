73b6e6f678dae61fd3e4009db6b1b87b1e531bac
niharika2211 <niharika2211@gmail.com>
2024-04-25 16:14:31 +0530

    11-functions.sh

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,5 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+ then
+     echo "not a root user"
+    exit 1 
+ else 
+    echo "yes you are a root user"
+fi
+
+VALIDATE { exit status=$1 success=$2 } 
 
 
+dnf install mysql -y
+
+VALIDATE {( $?  installing mysql )}
+
+dnf install git -y
+
+VALIDATE {( $?  installing git )}
 
 
 
